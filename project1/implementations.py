c416e406fd2cf3f1b651c24c594bc850c687c8ee
Pucci <federico.pucci2@studio.unibo.it>
2017-10-21 11:40:14 +0200

    updated notebook

--- project1/implementations.py
+++ project1/implementations.py
@@ -131,9 +131,9 @@ def stochastic_gradient_descent(y, tx, initial_w, batch_size, max_iters, gamma):
 
 def standardize(x):
     """Standardize the original data set."""
-    mean_x = np.mean(x)
+    mean_x = np.nanmean(x, axis=0)
     x = x - mean_x
-    std_x = np.std(x)
+    std_x = np.std(x, axis=0)
     x = x / std_x
     return x, mean_x, std_x
 
