56e2a3806f6018a1247670ff2dace2aef0535a39
Pucci <federico.pucci2@studio.unibo.it>
2017-12-05 16:24:10 +0100

    added preprocessing file

---
+++ project2/preprocessing/dnc_parser.py
@@ -0,0 +1,10 @@
+
+class DNCParser:
+
+    method = None
+    def __init__(self, method):
+        self.method  = method
+
+
+    def parse_single_sentence(self, single_sentence):
+        return single_sentence
