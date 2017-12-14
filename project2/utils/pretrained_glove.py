98c5eddba331a26d300d5214a299a47d8e825d86
Pucci <federico.pucci2@studio.unibo.it>
2017-12-14 20:04:25 +0100

    fixed nonascii

--- project2/utils/pretrained_glove.py
+++ project2/utils/pretrained_glove.py
@@ -14,8 +14,7 @@ class GloveTrainer:
 
     def generate_word_embeddings(self):
         print('Indexing word vectors.')
-        f = open('./glove.twitter.27B/glove.twitter.27B.'+str(self.vector_size)+'d.txt')
-        print(f)
+        f = open('./glove.twitter.27B/glove.twitter.27B.'+str(self.vector_size)+'d.txt', encoding="utf-8")
         for line in f:
             values = line.split()
             word = values[0]
